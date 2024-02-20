d114afa1dfb9be5fefa50e34e79a32d68d0335e3
Yumaguen Timur <timur_red82@mail.ru>
2024-02-20 11:45:34 +0300

    Введите число на проверку кратности 7 и 23

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num % 7) == 0 && (num % 23) == 0)
+{
+    Console.Write(true);
+}
+else
+{
+    Console.Write(false);
+}
