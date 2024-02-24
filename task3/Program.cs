78e7058f1c719bbc076c0f2a6c2bae4780fd1a75
Yumaguen Timur <timur_red82@mail.ru>
2024-02-24 12:48:19 +0300

    наибольшаясцифра числа из [10:99]

---
+++ task3/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+//  и показывает наибольшую цифру числа.
+
+Console.Write("Веедите число от 10 до 99 :  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num1 = num%10;
+int num2 = num/10;
+
+// if (num1 == num2)
+// {
+//     Console.Write(Цифры числа равны);
+// }
+if (num1 >= num2)
+{
+    Console.Write(num1);
+}
+else if(num2 >= num1)
+{
+    Console.Write(num2);
+}
+
