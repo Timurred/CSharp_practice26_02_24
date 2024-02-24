8384c404fc062621a5d9ab41a1346ec94c2f7d53
Yumaguen Timur <timur_red82@mail.ru>
2024-02-24 12:50:08 +0300

    разбить число на цифры через запятую

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+Console.Write("Введите натуральное число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+int quantity = (int)Math.Log10(num);
+int index = 0;
+while (index< quantity)
+{
+int currentnum = (int)(num / Math.Pow(10, quantity - index))%10;
+Console.Write($"{currentnum}, ");
+index++; 
+}
+Console.Write($"{num%10}");
+ 
